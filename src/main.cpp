bc0b79e87e1b810e28794f281d4a396394b9e417
Michael DiGregorio <michael.c.digregorio@gmail.com>
2023-03-16 07:44:03 -0400

    LearnOpenGL - Hello Triangle

--- src/main.cpp
+++ src/main.cpp
@@ -33,6 +33,7 @@ int main()
     }
     
     glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     
     if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
@@ -40,9 +41,90 @@ int main()
         return -1;
     }
     
-    glViewport(0, 0, 800, 600);
+    //I originally had a viewport call here.
+    //If added, quad shows up towards bottom left then centers after resizing window
+    //Figure out why
+    //Resize fires framebuffer_size_callback
+    //glViewport(0, 0, 800, 600);
     
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    const char *vertexShaderSource = "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "}\0";
+
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "Error during vertex shader compilation: " << infoLog << std::endl;
+    }
+    
+    const char *fragmentShaderSource = "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "void main()\n"
+        "{\n"
+        "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+        "}\0";
+    
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if(!success)
+    {
+        glGetProgramInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "Error during shader linking: " << infoLog << std::endl;
+    }
+    
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    
+    float vertices[] = {
+         0.5f,  0.5f, 0.0f, //top right
+         0.5f, -0.5f, 0.0f, //bottom right
+        -0.5f, -0.5f, 0.0f, //bottom left
+        -0.5f,  0.5f, 0.0f  //top left
+    };
+    
+    unsigned int indices[] = {
+        0, 1, 3, //first triangle
+        1, 2, 3  //second triangle
+    };
+    
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+    
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
     
     while(!glfwWindowShouldClose(window))
     {
@@ -52,6 +134,12 @@ int main()
         //render
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        //Optional, unbind
+        glBindVertexArray(0);
         
         //check and call events, swap buffers
         glfwSwapBuffers(window);
