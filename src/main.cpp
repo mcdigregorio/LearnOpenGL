82c2c1321a91ce268883a7ac44f5db9ccd48e0a2
Michael DiGregorio <michael.c.digregorio@gmail.com>
2023-03-17 07:28:35 -0400

    LearnOpenGL - Hello Triangle, exercise 1

--- src/main.cpp
+++ src/main.cpp
@@ -98,10 +98,13 @@ int main()
     glDeleteShader(fragmentShader);
     
     float vertices[] = {
-         0.5f,  0.5f, 0.0f, //top right
-         0.5f, -0.5f, 0.0f, //bottom right
-        -0.5f, -0.5f, 0.0f, //bottom left
-        -0.5f,  0.5f, 0.0f  //top left
+        -0.75f, -0.5f, 0.0f,
+         0.0f,  -0.5f, 0.0f,
+        -0.35f,  0.5f, 0.0f,
+        
+         0.0f,  -0.5f, 0.0f,
+         0.75f, -0.5f, 0.0f,
+         0.35f,  0.5f, 0.0f
     };
     
     unsigned int indices[] = {
@@ -121,8 +124,8 @@ int main()
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    //glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
     
@@ -137,7 +140,8 @@ int main()
         
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
         //Optional, unbind
         glBindVertexArray(0);
         
